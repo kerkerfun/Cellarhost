db471a67419e9c5b05f1ce2fec5ca1b1818e2b14
shanehuang <borderun1010@gmail.com>
2017-06-01 10:27:43 +0800

    fix page

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,34 +1,33 @@
 class CartItemsController < ApplicationController
-   before_action :authenticate_user!
+    before_action :authenticate_user!
 
-   def destroy
-     @cart = current_cart
-     @cart_item = @cart.cart_items.find_by(product_id: params[:id])
-     @product = @cart_item.product
-     @cart_item.destroy
+    def destroy
+        @cart = current_cart
+        @cart_item = @cart.cart_items.find_by(product_id: params[:id])
+        @product = @cart_item.product
+        @cart_item.destroy
 
-     flash[:warning] = "成功将 #{@product.title} 从购物车删除！"
-     redirect_to :back
-   end
+        flash[:warning] = "成功將 #{@product.title} 從購物車删除！"
+        redirect_to :back
+    end
 
-   def update
-     @cart = current_cart
-     @cart_item = @cart.cart_items.find_by(product_id: params[:id])
+    def update
+        @cart = current_cart
+        @cart_item = @cart.cart_items.find_by(product_id: params[:id])
 
-     if @cart_item.product.quantity >= cart_item_params[:quantity].to_i
-        @cart_item.update(cart_item_params)
-        flash[:notice] = "成功变更数量"
-     else
-        flash[:warning] = "数量不足以加入购物车"
-     end
-     
+        if @cart_item.product.quantity >= cart_item_params[:quantity].to_i
+            @cart_item.update(cart_item_params)
+            flash[:notice] = '成功變更數量'
+        else
+            flash[:warning] = '數量不足以加入購物車'
+        end
 
-     redirect_to carts_path
-   end
+        redirect_to carts_path
+    end
 
-   private
+    private
 
-   def cart_item_params
-     params.require(:cart_item).permit(:quantity)
-   end
+    def cart_item_params
+        params.require(:cart_item).permit(:quantity)
+    end
 end
