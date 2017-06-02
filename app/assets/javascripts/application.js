c1a3ccc8856c46abb24adbbad44205dfed4042b3
shanehuang <borderun1010@gmail.com>
2017-06-02 13:19:34 +0800

    navbar changed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,6 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap/alert
-//= require bootstrap/dropdown
 //= require_tree .
 $(window).scroll(function () {
   if ($(this).scrollTop() > 125) {
